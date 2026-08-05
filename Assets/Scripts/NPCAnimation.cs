408cccbc6d937438ec9000bf65cd44332becad6a
shizushuuu <shizushuuu@gmail.com>
2026-08-05 13:27:22 +0800

    Adding Chibi model, animation idle and walk
    added npc model, did animation for chibi idle and chibi walk for npchibi

---
+++ Assets/Scripts/NPCAnimation.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NPCAnimation : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+
+        bool isWalking =
+        agent.velocity.sqrMagnitude > 0.01f &&
+        !agent.pathPending &&
+        !agent.isStopped;
+
+        animator.SetBool("IsWalking", isWalking);
+    }
+
+    public void Grab()
+    {
+        animator.SetTrigger("Grab");
+    }
+
+    public void Sit()
+    {
+        animator.SetTrigger("Sit");
+    }
+
+    public void Wave()
+    {
+        animator.SetTrigger("Look");
+    }
+}
